fb46c5cab711984175a52e312eef0d93b05df422
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-04-09 14:56:42 +0100

    Gives price for complex entry

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,25 +1,58 @@
 class Shop
 
-  attr_reader :price_table
-
-  def initialize(price_table)
+  def initialize(price_table, special_offer = nil)
     @price_table = price_table
+    @special_offer = special_offer 
   end
 
   def checkout(units)
-    unit_prices = []
-    units.split('').each do |unit|
-      return -1 if illegal?(unit)
-      unit_prices << price_table[unit.to_s]
-    end
-    print unit_prices
-    unit_prices.inject(:+)
+    @total = 0
+    return -1 if  illegal_entry?(units)
+    calculate_price(units)
   end
 
   private
 
-  def illegal?(unit)
-    price_table[unit.to_s].nil?
+  attr_reader :price_table, :special_offer
+
+  def calculate_price(units)
+    sorted_units = sort_units(units)
+    sorted_units.each do | unit, quantity |
+      updated_quantity = implement_offer(unit, quantity)
+      @total += price_table[unit] * updated_quantity
+    end 
+    @total
+  end
+
+  def implement_offer(unit, quantity)
+    special_offer.each do | name, reduced_price |
+      discounted_unit = name.split('')[1]
+      discounted_quantity =  name.split('')[0].to_i
+      if unit == discounted_unit && quantity % discounted_quantity == 0
+        loop do 
+        @total += reduced_price 
+        quantity -= discounted_quantity
+        break if quantity < discounted_quantity
+        end
+      end
+    end
+    quantity
+  end  
+
+  def update_quantity
+
+  end
+
+  def sort_units(units)
+    sorted_units = {}
+    units.split('').map do |unit|
+      sorted_units[unit].nil? ? sorted_units[unit] = 1 : sorted_units[unit] += 1
+    end
+    sorted_units
+  end
+
+  def illegal_entry?(units)
+    units.split('').map { |unit| price_table[unit].nil? }.inject(:|)
   end
  
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -9,34 +9,59 @@ describe Shop do
     'D' => 15
   }
 
-subject(:shop) { described_class.new(price_table) }
+  special_offer = {
+    '3A' => 130,
+    '2B' => 45
+  }
+subject(:shop) { described_class.new(price_table, special_offer) }
 
-  it 'gives a price for A' do 
-    price = 50
-    expect(shop.checkout('A')).to eq price 
+  it 'gives a price of 50 for single legal item A ' do 
+    expect(shop.checkout('A')).to eq 50 
   end
 
-  it 'gives a price for B' do 
-    price = 30
-    expect(shop.checkout('B')).to eq price 
-  end
-
-  it 'gives a price for C' do 
-    price = 20
-    expect(shop.checkout('C')).to eq price 
-  end
-
-  it 'gives a price for D' do 
-    price = 15
-    expect(shop.checkout('D')).to eq price 
-  end
-
-  it 'gives -1 for an illegal input 6' do 
+  it 'Acceptance criteria: gives -1 for an illegal input 6' do 
     expect(shop.checkout('6')).to eq -1 
   end
 
-  it 'gives 115 for multiple legal entry' do
+  it 'Acceptance criteria: gives 115 for multiple legal entry' do
     expect(shop.checkout('ABCD')).to eq 115
   end
 
+  it 'Acceptance criteria: gives -1 for aBc' do 
+    expect(shop.checkout('aBc')).to eq -1
+  end
+
+  it 'Acceptance criteria: gives -1 for -B8x' do 
+    expect(shop.checkout('-B8x')).to eq -1
+  end
+
+  it 'Acceptance criteria: gives -1 for 18' do 
+    expect(shop.checkout('18')).to eq -1
+  end
+
+  it 'Acceptance criteria: gives 100 for AA' do 
+    expect(shop.checkout('AA')).to eq 100
+  end
+
+  it 'Acceptance criteria: gives 130 for -B8x' do 
+    expect(shop.checkout('AAA')).to eq 130
+  end
+
+  it 'Acceptance criteria: gives 260 for AAAAAA' do 
+    expect(shop.checkout('AAAAAA')).to eq 260
+  end
+
+  it 'Extra acceptance criteria: gives 175 for AAABB' do 
+    expect(shop.checkout('AAABB')).to eq 175
+  end
+
+  it 'Extra acceptance criteria: gives 195 for AAABBC' do 
+    expect(shop.checkout('AAABBC')).to eq 195
+  end
+
+  it 'Extra acceptance criteria: gives 345 for BABDBCAACA' do 
+    expect(shop.checkout('BABDBCAACA')).to eq 345
+  end
+
 end
+
